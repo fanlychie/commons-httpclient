24eaaee4abf13179e496a8005f6b3f2071e6bc5c
fanlychie <fanlychie@yeah.net>
2017-01-27 15:42:12 +0800

    内容类型常量

---
+++ src/main/java/org/fanlychie/commons/httpclient/ContentTypeConstant.java
@@ -0,0 +1,27 @@
+package org.fanlychie.commons.httpclient;
+
+import org.apache.http.Consts;
+import org.apache.http.entity.ContentType;
+
+/**
+ * 内容类型常量
+ * Created by fanlychie on 2017/1/27.
+ */
+public interface ContentTypeConstant {
+
+    /**
+     * text/html utf-8 content type
+     */
+    ContentType TEXT_HTML = ContentType.create("text/html", Consts.UTF_8);
+
+    /**
+     * application/json utf-8 content type
+     */
+    ContentType APPLICATION_JSON = ContentType.create("application/json", Consts.UTF_8);
+
+    /**
+     * application/x-www-form-urlencoded utf-8 content type
+     */
+    ContentType APPLICATION_FORM_URLENCODED = ContentType.create("application/x-www-form-urlencoded", Consts.UTF_8);
+
+}
